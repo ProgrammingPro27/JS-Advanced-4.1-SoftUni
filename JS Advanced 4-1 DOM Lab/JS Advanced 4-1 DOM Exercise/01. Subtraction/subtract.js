e780423fee636b01c3eb55e8c9271d076b6894fa
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2021-03-15 10:45:41 +0200

    Delete JS Advanced 4-1 DOM Lab/JS Advanced 4-1 DOM Exercise directory

--- JS Advanced 4-1 DOM Lab/JS Advanced 4-1 DOM Exercise/01. Subtraction/subtract.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    let firstNumber = Number(document.getElementById("firstNumber").getAttribute("value"));
-    let secondNumber = Number(document.getElementById("secondNumber").getAttribute("value"));
-    let resultArea = document.getElementById("result");
-
-    resultArea.innerHTML = firstNumber - secondNumber;
-
-}
